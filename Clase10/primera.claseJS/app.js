6bc64955103f022c9c205bd2f5c64d1073dbd1b3
Camila <kamilacastillo609@gmail.com>
2022-12-20 23:54:04 -0300

    tema nuevo js

---
+++ Clase10/primera.claseJS/app.js
@@ -0,0 +1,106 @@
+//es un comentario en una sola linea
+
+/* mas de 
+una
+linea */
+
+
+// declaraion de variables
+// tipos de datos
+
+/*
+numericos -1 .... 10000 ,reales 2.90 5.01
+cadena de caracteres(string) hola mundo pedro listado l o T
+valores logicos (true y false) (1 y 0)
+*/
+/*var numero = 96(no se usa 'viejo')
+
+let palabras = 'pedro'
+
+const numeroDePi = 3.1416
+
+let esNuevo = true
+
+
+let numero = 101
+
+let numero = 63
+console.log('el valor inicial de numero es ', numero)
+
+let nuevoNumero = 987987987987987 
+console.log('el valor de nuevoNumero es',nuevoNumero)
+
+nuevoNumero = numero
+
+console.log('este es el valor que tiene la ultima variable', nuevoNumero)
+*/
+
+let nombre='juan pedro';
+let edad=36;
+let estadoCivil='casado';
+let programa=false;
+
+//NO SE USAconsole.log('tu nombre es',nombre,'y tu edad es',edad,'años','y tu estado civil',estadoCivil) 
+
+//NO SE USA console.log('tu nombre es'  +  nombre  +  'y tu edad es'  +  edad  + 'años'  +  'y tu estado civil' + estadoCivil)
+
+// template string
+
+// console.log(`tu nombre es ${nombre} y tu edad es  ${edad} años y tu estado civil es ${ estadoCivil}`)
+
+// entrada de datos por teclado
+
+//let guardoNombre = prompt('ingrese su nombre')
+ 
+//console.log(guardoNombre)
+//let guardoEdad = prompt('ingrese su edad')
+/*let numero1=0
+console.log('el valor inicial es de tipo',typeof(numero1))
+let numero2=0
+
+
+numero1 =parseInt (prompt('ingrese el primer numero'))
+console.log(typeof(numero1))
+numero2 = parseInt(prompt('ingrese segundo numero'))*/
+
+//let resultadoMulti =numero1*numero2
+//let resultadoDivi = numero1 % numero2
+//let resultadoResta=numero1-numero2
+//let resultadoSuma = numero1 + numero2
+//console.log(resultadoSuma)
+
+//estructura condicional simple
+let notaAprobado=5
+
+let nota=parseInt(prompt('ingrese la nota del alumno'))
+
+//if( nota >= 5   ){
+ //           console.log('aprobo la materia')
+//}
+
+//estructura condicional compuesta
+//let nota=parseInt(prompt('ingrese la nota del alumno'))
+
+//if( nota >= 5){
+//           console.log('aprobo la materia')
+//}else{
+//   console.log('no aprobaste la materia')
+//}
+//operador ternario
+
+nota>=notaAprobado ? console.log('aprobo la materia') : console.log('no aprobo la materia') // muy recomendable
+
+
+
+
+
+//operadores relaciones
+/*
+>mayor
+>=mayor igual
+<menor
+<=menor igual
+==igual igual
+=== estrictamente igual
+
+*/
